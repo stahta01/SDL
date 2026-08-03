95826833efc4ea8cd247b99c64ad80db79724e4d
Tim Stahlhut <stahta01@gmail.com>
2026-08-03 08:52:20 -0400

    Add support for MSYS
    [sdl-ci-filter cygwin]
    [sdl-ci-filter msys2-*]
    [sdl-ci-filter msvc-*]
    [sdl-ci-filter ngage]
    [sdl-ci-filter steamrt4]

--- src/io/SDL_iostream_c.h
+++ src/io/SDL_iostream_c.h
@@ -23,7 +23,7 @@
 #ifndef SDL_iostream_c_h_
 #define SDL_iostream_c_h_
 
-#if defined(SDL_PLATFORM_WINDOWS) && !defined(SDL_PLATFORM_CYGWIN)
+#if defined(SDL_PLATFORM_WINDOWS) && !defined(SDL_PLATFORM_CYGWIN) || defined(__MSYS__)
 SDL_IOStream *SDL_IOFromHandle(HANDLE handle, const char *mode, bool autoclose);
 #else
 #if defined(HAVE_STDIO_H)
